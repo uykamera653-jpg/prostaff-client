8305b440446d7131fffd860a62c6a16dad82166f
uykamera653-jpg <uykamera653@gmail.com>
2025-09-26 18:04:33 +0500

    Update env.js

--- env.js
+++ env.js
@@ -1,1 +1,12 @@
-window.__ENV={}
+// env.js
+window.__ENV = {
+  apiKey: "AIzaSyBUOLvqsw7SS0qL8fjLRRelpey65otHhsg",
+  authDomain: "prostaff-prod.firebaseapp.com",
+  projectId: "prostaff-prod",
+  storageBucket: "prostaff-prod.firebasestorage.app",
+  messagingSenderId: "468406236145",
+  appId: "1:468406236145:web:ecfebea79ac0077b704a50",
+  measurementId: "G-MQPQLKGJB3",
+  // Web Push bo'lsa keyin qo'shamiz:
+  // vapidKey: "BGjiRHadFPF... (Public VAPID)"
+};
