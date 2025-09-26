f9a89f0ff760f40d0a8fca044fa352fe8e0ae6bb
uykamera653-jpg <uykamera653@gmail.com>
2025-09-26 18:22:54 +0500

    Update env.js

--- env.js
+++ env.js
@@ -1,4 +1,4 @@
-// env.js
+// --- PROSTAFF: PROD muhit ---
 window.__ENV = {
   apiKey: "AIzaSyBUOLvqsw7SS0qL8fjLRRelpey65otHhsg",
   authDomain: "prostaff-prod.firebaseapp.com",
@@ -7,6 +7,25 @@ window.__ENV = {
   messagingSenderId: "468406236145",
   appId: "1:468406236145:web:ecfebea79ac0077b704a50",
   measurementId: "G-MQPQLKGJB3",
-  // Web Push bo'lsa keyin qo'shamiz:
-  // vapidKey: "BGjiRHadFPF... (Public VAPID)"
+
+  // Web Push (VAPID public key) — siz import qilgan prod kalit
+  vapidKey:
+    "BGjiRHadFPFMmOZDa1g-GI609vAyDapiMvUN9JEsz4qN_nNGbnVePivaGHWBesydr4wPpx4v69roZMozH2RrPdk",
 };
+
+/* Agar keyin DEV sayt ham bo‘lsa, shunchaki tepadagini o‘chirib-yoqish uchun
+   mana buni vaqtincha izohda qoldiryapman:
+
+// --- DEV muhit ---
+window.__ENV = {
+  apiKey: "AIzaSyBnv2KFyK2d_yoz07c7_CpZY4YKxCNL2eQ",
+  authDomain: "prostaff-dev.firebaseapp.com",
+  projectId: "prostaff-dev",
+  storageBucket: "prostaff-dev.firebasestorage.app",
+  messagingSenderId: "113655418869",
+  appId: "1:113655418869:web:32f0c2fe163f3264a5dabc",
+  measurementId: "G-Y240BN3SG3",
+  vapidKey:
+    "BFrNlM17gfdZd1AvXEs7Z6LBnoWJgER8xIpVNCh_cU37GAZ_Q4QyPFtqJeZ-qlGb3nHl_hHXZtTz5RgCDOlLzPA",
+};
+*/
